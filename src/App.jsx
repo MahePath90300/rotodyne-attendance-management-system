867d4c752664e016724a6bc3ed6c5ca18f3c9420
MahePath90300 <mahender.fedev32@gmail.com>
2025-11-26 16:35:10 +0530

    Login page UI component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Login from './components/Login/Login'
 
 function App() {
 
   return (
     <div>
-      <h1>Rotodyne Attendance Management System</h1>
+     <Login/>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import tailwindPostcss from "@tailwindcss/postcss";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindPostcss()],
+    },
+  },
 })
